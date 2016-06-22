65e5245d15e8027af74796498078baf6e02e770d
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-06-22 17:28:43 -0400

    only include shifts that dialect _helps_ with
    remove caught cot merger and add caught-cot non-mergers

--- data-raw/clopper_competitors.R
+++ data-raw/clopper_competitors.R
@@ -6,20 +6,21 @@ North,ae,eh,Northern cities shift
 North,eh,uh,Northern cities shift
 North,uh,ao,Northern cities shift
 North,aa,ae,Northern cities shift
-South,eh,ey,Southern chain shift
-South,ey,eh,Southern chain shift
-South,ey,ih,Southern chain shift
-South,ih,iy,Southern chain shift
+North,ao,aa,Caught-cot non-merger
+North,aa,ao,Caught-cot non-merger
+South,eh,ey,EH tensing
+South,ey,eh,EY laxing
+South,ey,ih,EY laxing
+South,ih,iy,IH tensing
+South,iy,ih,IY laxing
 South,ow,oo,OW fronting
-Midland,aa,ao,Caught-cot merger
+South,ao,aa,Caught-cot non-merger
+South,aa,ao,Caught-cot non-merger
 Mid-Atlantic,ao,ow,AO raising
 Mid-Atlantic,ao,uh,AO raising
-Mid-Atlantic,ao,aa,Caught-cot merger
-Mid-Atlantic,aa,ao,Caught-cot merger
-West,aa,ao,Caught-cot merger
-West,ao,aa,Caught-cot merger
-New England,aa,ao,Caught-cot merger
-New England,ao,aa,Caught-cot merger" %>%
+Mid-Atlantic,ao,aa,Caught-cot non-merger
+Mid-Atlantic,aa,ao,Caught-cot non-merger
+" %>%
   read_csv() ->
   clopper_competitors
 
