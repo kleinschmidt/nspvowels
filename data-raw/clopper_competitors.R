d648763ac1f9926817cd3e54677d9ad1ba3dae7d
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-06-22 17:10:53 -0400

    additional comparisons and descpription to compet.

--- data-raw/clopper_competitors.R
+++ data-raw/clopper_competitors.R
@@ -1,20 +1,25 @@
+library(magrittr)
 library(readr)
 
-"Dialect,Vowel,Competitor
-North,ae,eh
-North,eh,uh
-North,uh,ao
-North,aa,ae
-South,eh,ey
-South,ey,eh
-South,ey,ih
-South,ih,iy
-South,ow,oo
-Midland,aa,ao
-Mid-Atlantic,ao,ow
-Mid-Atlantic,ao,uh
-West,aa,ao
-New England,aa,ao" %>%
+"Dialect,Vowel,Competitor,Description
+North,ae,eh,Northern cities shift
+North,eh,uh,Northern cities shift
+North,uh,ao,Northern cities shift
+North,aa,ae,Northern cities shift
+South,eh,ey,Southern chain shift
+South,ey,eh,Southern chain shift
+South,ey,ih,Southern chain shift
+South,ih,iy,Southern chain shift
+South,ow,oo,OW fronting
+Midland,aa,ao,Caught-cot merger
+Mid-Atlantic,ao,ow,AO raising
+Mid-Atlantic,ao,uh,AO raising
+Mid-Atlantic,ao,aa,Caught-cot merger
+Mid-Atlantic,aa,ao,Caught-cot merger
+West,aa,ao,Caught-cot merger
+West,ao,aa,Caught-cot merger
+New England,aa,ao,Caught-cot merger
+New England,ao,aa,Caught-cot merger" %>%
   read_csv() ->
   clopper_competitors
 
